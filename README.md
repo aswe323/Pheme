4c053efa2b70c50a5798dd6d62dcbe26300b16a5
aswe323 <matanwx135@gmail.com>
2025-01-27 18:57:43 +0200

    added text highliting based on word list

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is inteded as a project to create a news utility.
 
+files:
+rss_links.txt - each line should be a URL to a RSS feed
+alerting_words - each line should be a word that would make the corresponding message that contains said word to be printed in red.
+
 dev:
     requires python3
                 html2text
--- main.py
+++ main.py
@@ -2,10 +2,32 @@ import time
 import feedparser
 import html2text
 
+# if the word is identified, the entire message will be printed red
+
+alerting_words = []
+def init_alerting_words(filename_path):
+    with open(filename_path, "r")as f:
+        for line in f:
+            line_no_newline = line.replace("\n", "")
+            alerting_words.append(line_no_newline)
+color = {
+        "RED":'\033[31m',
+        "GREEN":'\033[32m',
+        'WHITE':'\033[37m'
+        }
+
+def pre_print(text, list_of_words):
+    returned = text
+    for word in list_of_words:
+        if word in text:
+            return f"{color['RED']}{text}{color['WHITE']}"
+    return text
+
 def main():
-    #feeds = {title, published}
     feeds = {}
-    flag = 0
+    #assuming the file exists, RTFM
+    init_alerting_words("trigger_words.txt")
+    print(alerting_words)
     while True:
         with open("rss-links.txt","r") as links_rss:
             for line in links_rss:
@@ -13,26 +35,21 @@ def main():
                 feeded = feedparser.parse(line)
                 title = feeded.entries[0].title
                 date_published = feeded.entries[0].published
-                #print(feeds.keys())
                 if feeded.feed.title not in feeds.keys():
                     feeds[feeded.feed.title] = ""
-
-               # print("###")
-               # print(feeds)
-               # print(date_published)
-               # print(f"{feeds[feeded.feed.title]} == {date_published}")
-               # print(feeds[feeded.feed.title] == date_published)
-               # print(feeded.feed.title in feeds.keys())
-                if feeded.feed.title in feeds.keys() and feeds[feeded.feed.title] == date_published:
+                if feeded.feed.title in feeds.keys() and feeds[feeded.feed.title] == html2text.html2text(feeded.entries[0].description):
+                    time.sleep(10)
                     pass
 
                 elif feeded.feed.title in feeds.keys() :
-                    print("###################")
                     print(feeded.feed.title)
+                    print(date_published)
                     print(feeded.entries[0].title)
-                    print(html2text.html2text(feeded.entries[0].description))
-                    feeds[feeded.feed.title] = date_published
+                    not_functional_var = html2text.html2text(feeded.entries[0].description)
+                    print(pre_print(not_functional_var,alerting_words))
+                    feeds[feeded.feed.title] = html2text.html2text(feeded.entries[0].description)
                     print("###################")
+
 
 
 if __name__ == "__main__":
